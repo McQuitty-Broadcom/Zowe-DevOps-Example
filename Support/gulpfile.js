d9a431ef3dac83534e85c3d3e5189b1464d96865
User1 <none@none.com>
2020-10-20 12:53:46 -0500

    Fix for password

--- Support/gulpfile.js
+++ Support/gulpfile.js
@@ -61,7 +61,7 @@ function createAndSetProfiles(host, user, pass, callback){
       dir: "command-archive/set-endevor-location-profile"
     },
     {
-      command: `zowe profiles create cics zw --host  ${host} --user ${user} --password ${password}` +
+      command: `zowe profiles create cics zw --host  ${host} --user ${user} --password ${pass}` +
                ` --port ${config.cicsPort} --region-name ${config.cicsRegion}` +
                ` --protocol ${config.cicsProtocol} --ru ${config.cicsRejectUnauthorized} --ow`,
       dir: "command-archive/create-cics-profile"
